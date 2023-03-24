0399c2ecef1013306472360402d3dc47f5288f11
noearobles <roblesnoe1030@gmail.com>
2023-03-23 21:10:00 -0500

    added .env and pool

---
+++ src/js/pool.mjs
@@ -0,0 +1,24 @@
+// const mysql = require('mysql2')
+
+// class Connection {
+//   constructor() {
+//     if (!this.pool) {
+//       console.log('creating connection...')
+//       this.pool = mysql.createPool({
+//         connectionLimit: 100,
+//         host: '',
+//         user: '',
+//         password: '',
+//         database: ''
+//       })
+
+//       return this.pool
+//     }
+
+//     return this.pool
+//   }
+// }
+
+// const instance = new Connection()
+
+// module.exports = instance;
