8aad7c7049f824c1cf8ba9438a7d3a5efcb3ac78
Kevin Huang <kevwhuang@gmail.com>
2023-03-23 22:17:23 -0500

    Merged all changes

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,7 @@ import express from 'express';
 import path from 'path';
 import url from 'url';
 
-// import routerEmployees from './routers/employees';
+// import routerEmployees from './routes/employees';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
