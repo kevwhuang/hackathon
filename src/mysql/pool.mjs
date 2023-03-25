0fee2cc1d27d9058c8cb3152e55ee362055f25f7
noearobles <roblesnoe1030@gmail.com>
2023-03-25 13:03:50 -0500

    added error logic and fixed port to 5000, I see what you meant Kevin

--- src/mysql/pool.mjs
+++ src/mysql/pool.mjs
@@ -1,24 +1,25 @@
-// const mysql = require('mysql2')
+import { HOST, DATABASE, PASSWORD, USERNAME } from "dotenv/config";
+import mysql from "mysql2";
 
-// class Connection {
-//   constructor() {
-//     if (!this.pool) {
-//       console.log('creating connection...')
-//       this.pool = mysql.createPool({
-//         connectionLimit: 100,
-//         host: '',
-//         user: '',
-//         password: '',
-//         database: ''
-//       })
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log("creating connection...");
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: HOST,
+        user: USERNAME,
+        password: PASSWORD,
+        database: DATABASE,
+      });
 
-//       return this.pool
-//     }
+      return this.pool;
+    }
 
-//     return this.pool
-//   }
-// }
+    return this.pool;
+  }
+}
 
-// const instance = new Connection()
+const instance = new Connection();
 
-// module.exports = instance;
+export { instance };
