8270b9c72a6f460749e142ba607b69ef40dd4360
ConnorMcGehee <connorjames050@gmail.com>
2023-03-28 20:13:42 -0500

    Fixed API endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -6,8 +6,6 @@ import url from 'url';
 
 import router from './routes/employees.js';
 
-// import routerEmployees from './routes/employees';
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __filename = url.fileURLToPath(import.meta.url);
@@ -24,11 +22,9 @@ const log = (req, res, next) => {
 
 app.listen(PORT, () => console.log('\x1b[35m%s\x1b[0m', `Server listening on PORT ${PORT}.`));
 
-app.use('/', router)
+app.use('/api', router)
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(log);
-
-// app.use('/employees', routerEmployees);
