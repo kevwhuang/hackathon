455f5dc6dd34ab2209d589e88c1132998527e749
ConnorMcGehee <connorjames050@gmail.com>
2023-03-28 20:07:44 -0500

    Fixed get by  ID and multiple parameters

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -4,13 +4,13 @@ const router = express.Router()
 
 router.get("/", employeesController.getEmployees);
 
-router.get("/:id", employeesController.getEmployeesById);
+router.get("/:emp_no", employeesController.getEmployeesById);
 
 router.get("/firstname/:first_name", employeesController.getEmployeesByFirstName);
 
-router.get("/salary/:id", employeesController.getSalaryById);
+router.get("/salary/:emp_no", employeesController.getSalaryById);
 
-router.get("/salary/firstname/:first_name", employeesController.getSalaryByName);
+router.get("/salary/:first_name&:last_name", employeesController.getSalaryByName);
 
 router.get("/department/", employeesController.getEmployeesByDepartment);
 
